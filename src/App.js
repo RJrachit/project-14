b2754d9c0c084f5cbf39e9d6869bf35bc86e4517
rachit.jaluthra <rachit.jaluthra@infoedge.com>
2023-02-05 00:16:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Rachit & Jasleen
         </a>
       </header>
     </div>
